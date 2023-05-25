82471577b4e56a2251351a20503c95f1e53ab445
Wuonam <odeedward04@gmail.com>
2023-05-25 15:04:52 +0200

    Exception1

---
+++ src/Exceptions/Exception1.java
@@ -0,0 +1,43 @@
+package Exceptions;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Exception1 {
+    
+
+        public static void main(String[] args) {
+            ExceptionTemp obj = new ExceptionTemp();
+
+		/*System.out.println("something");
+		System.out.println("something");
+		method(null);              //exception here since you cant pass a null object to the method
+		System.out.println("something");
+		System.out.println("something");*/
+            newMethRec(); 
+        }
+
+        static void newMethRec(){
+            try {                                    //file might not be [resent so compiler throws a file not found exception
+                FileReader file = new FileReader("C:\\Users\\ODERA\\Desktop");
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            //newMethRec();   //Method calling itself recursively leading to stack overflow
+        }
+
+        static void method(ExceptionTemp obj) {
+            method2(obj);
+        }
+
+        static void method2(ExceptionTemp obj) {
+            try {
+                System.out.println(obj.example());
+                System.out.println(obj.example());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
---
+++ src/Exceptions/ExceptionTemp.java
@@ -0,0 +1,15 @@
+package Exceptions;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+public class ExceptionTemp {
+    
+        public String example() {
+            return "example method called";
+        }
+
+        public void method2(ExceptionTemp obj) throws NullPointerException{
+
+            obj.example();
+
+        }
+    }
